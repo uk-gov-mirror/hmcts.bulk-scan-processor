821a43bfdbcfb0a58a90cf9ac6a934ecdcb8f3f5
gauravchawla <gaurav-chawla@hotmail.com>
2021-04-14 02:10:29 +0100

    commit for controller

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/ZipStatusController.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/ZipStatusController.java
@@ -3,17 +3,18 @@ package uk.gov.hmcts.reform.bulkscanprocessor.controllers;
 import javassist.tools.web.BadHttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
 import uk.gov.hmcts.reform.bulkscanprocessor.model.out.zipfilestatus.ZipFileStatus;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.zipfilestatus.ZipFileStatusService;
 
+import javax.management.BadAttributeValueExpException;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -34,20 +35,17 @@ public class ZipStatusController {
     // endregion
 
     @RequestMapping
-    public List<ZipFileStatus> findByFileNameOrDCN(@RequestParam(required = true) @NotNull @Size(max = 1) Map<String,String> params) throws HttpClientErrorException.BadRequest {
-
-        String fileName = params.get("fileName");
+    public ResponseEntity<List<ZipFileStatus>> findByFileNameOrDCN(@RequestParam Map<String,String> params){
+        String fileName = params.get("name");
         String dcn = params.get("dcn");
-
-        if (fileName != null) {
+        if (fileName != null && dcn == null) {
             List<ZipFileStatus> zipFileStatuses = new ArrayList<>();
             zipFileStatuses.add(service.getStatusFor(fileName));
-            return zipFileStatuses;
+            return ResponseEntity.ok().body(zipFileStatuses);
         }
-        else if (dcn!= null) {
-            return service.getStatusByDcn(dcn);
+        else if (dcn!= null && fileName == null) {
+            return ResponseEntity.ok().body(service.getStatusByDcn(dcn));
         }
-        return null;
+        return ResponseEntity.badRequest().body(null);
     }
-
 }
